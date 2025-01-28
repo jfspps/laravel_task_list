77d9dbbab0054a838749384ec2794f275db00737
James Apps <james.apps@energise.energy>
2025-01-28 14:17:45 +0000

    defined CSS classes, and applied to button tags and anchor tags

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,8 +5,9 @@
 @section('content')
 
     <nav class="mb-4">
-        <a href="{{ route('tasks.create') }}" class="font-medium text-gray-700 underline decoration-pink-500">Add
-            task</a>
+        <a href="{{ route('tasks.create') }}" class="link">
+            Add task
+        </a>
     </nav>
 
     @forelse($tasks as $theTask)
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,47 +6,48 @@
 @section('title', $task->title)
 
 @section('content')
-    <p>{{ $task->description }}</p>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.list') }}" class="link">Return to task list</a>
+    </nav>
+
+    <p class="mb-4 text-slate-700">{{ $task->description }}</p>
 
     @isset($task->long_description)
-        <p>{{ $task->long_description }}</p>
+        <p class="mb-4 text-slate-700">{{ $task->long_description }}</p>
     @endisset
+    {{-- Render the date relative to the present with diffForHumans() --}}
+    <p class=" mb-4 text-sm text-slate-500">Created: {{ $task->created_at->diffForHumans() }}</p>
+    <p class=" mb-4 text-sm text-slate-500">Updated: {{ $task->updated_at->diffForHumans() }}</p>
 
-    <p>Created at: {{ $task->created_at }}</p>
-    <p>Updated at: {{ $task->updated_at }}</p>
-
-    <p>
+    <p class="mb-4">
         @if($task->completed)
-            Status: Completed
+            <span class="font-medium text-green-500">Status: Completed</span>
         @else
-            Status: Not completed
+            <span class="font-medium text-red-500">Status: Not completed</span>
         @endif
     </p>
 
-    <div>
+    <div class="flex gap-2">
         {{-- Laravel will assume the entity has a primary key and use that when mapping to the endpoint --}}
-        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
-        <br/>
-        <br/>
-    </div>
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}" class="btn">
+            Edit
+        </a>
 
-    <div>
         <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
             @csrf
             @method('PUT')
 
-            <button type="submit">
+            <button type="submit" class="btn">
                 Mark as {{ $task->completed ? 'not completed' : 'completed' }}
             </button>
         </form>
-        <br/>
-    </div>
 
-    <div>
         <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="POST">
             @csrf
             @method('DELETE')
-            <button type="submit">Delete</button>
+            <button type="submit" class="btn">
+                Delete
+            </button>
         </form>
     </div>
 @endsection
